931eabcec6237411369ea77010a3ffff802e7d65
sKawashima <bysk.333@gmail.com>
2019-04-28 21:39:56 +0900

    setup

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div
+    #menu.navbar
+      .navbar-brand
+        nuxt-link(to='/').navbar-item
+          p Cocktail Menu
+    nuxt
+</template>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template lang='pug'>
+  section
+    div
+      logo
+      h1.title
+        | cocktailMenu
+      h2.subtitle
+        | meu test
+      .links
+        a.button--green(href="https://nuxtjs.org/" target="_blank") Documentation
+        a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank") GitHub
+        .button test
+
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+</style>
