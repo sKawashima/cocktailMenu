d7fc286253ccea97593b8119fd507d330e9d14ab
sKawashima <bysk.333@gmail.com>
2019-04-29 11:20:25 +0900

    comp

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,13 @@
 <template lang="pug">
   div
     nav.navbar.has-shadow
-      .navbar-brand.navbar-start
+      .navbar-brand
+        nuxt-link.navbar-item.has-text-centered(to='/')
+          font-awesome-icon(icon='cocktail').is-size-4
+        a.navbar-burger#burger(aria-label='menu' aria-expanded='false' @click='toggleBurger')
+          - for (let i = 0;i < 3;i++)
+            span(aria-hidden='true')
+      #mainMenu.navbar-menu.navbar-start(v-bind:class='[{"is-active": isActiveBurger}]')
         .navbar-item
           nuxt-link(to='/recipe')
             font-awesome-icon(icon='clipboard-list').is-size-5
@@ -10,16 +16,15 @@
           nuxt-link(to='/list')
             font-awesome-icon(icon='wine-bottle').is-size-5
             span Bottle
-      .navbar-item
-        font-awesome-icon(icon='cocktail').is-size-4
-      nuxt-link(to='/').navbar-end
+      #userSettings.navbar-menu.navbar-end(v-bind:class='[{"is-active": isActiveBurger}]')
         .navbar-item
           .buttons
             a.button(disabled) ユーザー認証
-    #main
+    .container
       nuxt
     footer.footer
-      p © 2019 sKawashima
+      .content.has-text-centered
+        p © 2019 sKawashima
 </template>
 
 <style scoped lang='sass'>
@@ -27,3 +32,30 @@
   span
     padding-left: 0.5em
 </style>
+
+<script lang="ts">
+import Vue from 'vue'
+
+let vm
+export default Vue.extend({
+  data: () => {
+    return {
+      isActiveBurger: 0
+    }
+  },
+  created() {
+    vm = this
+  },
+  methods: {
+    toggleBurger: () => {
+      console.log('clicked burger')
+      vm.isActiveBurger = ~vm.isActiveBurger
+      console.log(vm.isActiveBurger)
+    }
+  }
+})
+</script>
+
+function newFunction() {
+  return this;
+}
