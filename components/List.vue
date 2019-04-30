97d67acca2db40fc97bfc4ac0152e5f4ace2f77a
sKawashima <bysk.333@gmail.com>
2019-05-01 01:07:34 +0900

    refactList

--- components/List.vue
+++ components/List.vue
@@ -1,19 +1,10 @@
 <template lang="pug">
-#list
-  h2 新着カクテル
-  .list
-    - for(let i = 0;i < 8;i++)
-      ListItem.item
+section
+  - for(let i = 0;i < 8;i++)
+    ListItem.item
 </template>
 
 <style lang="sass" scoped>
-#list
-  margin-top: 2em
-  padding: 0 1em
-  h2
-    font-weight: bold
-    font-size: 1.5rem
-    margin: 0 0 1rem
 </style>
 
 <script lang="ts">
