99b0867398fb8860c283936969c651ec8e8dc214
sKawashima <bysk.333@gmail.com>
2019-05-01 00:57:26 +0900

    makeModules

--- components/List.vue
+++ components/List.vue
@@ -1,14 +1,28 @@
 <template lang="pug">
 #list
   h2 新着カクテル
-  p ここに新着がずらーってなる
+  .list
+    - for(let i = 0;i < 8;i++)
+      ListItem.item
 </template>
 
 <style lang="sass" scoped>
 #list
   margin-top: 2em
+  padding: 0 1em
   h2
     font-weight: bold
     font-size: 1.5rem
     margin: 0 0 1rem
 </style>
+
+<script lang="ts">
+import ListItem from '~/components/List_item.vue'
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+    ListItem
+  }
+})
+</script>
---
+++ components/List_item.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+a
+  img(src='http://www.suntory.co.jp/wnb/img/cocktail/p_Kahlua_milk.gif')
+  h3 カルーア・ミルク
+  p 12度
+  p 甘口
+  p カルーア・コーヒー・リキュール, 牛乳
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+})
+</script>
