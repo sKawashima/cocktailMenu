eae4b128d1b27b879aa290accf04f3fcc3a48eab
Shota Kawashima <bysk.333@gmail.com>
2019-07-22 23:30:58 +0900

    fixColor (#101)
    * feat:create pull request
    
    * changeColor

--- components/atoms/Input.vue
+++ components/atoms/Input.vue
@@ -9,9 +9,12 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+$theme: #2D1112
+
 .input
-  border: 1px solid #000
+  border: 1px solid $theme
   box-sizing: border-box
+  color: $theme
   font-size: 14px
   padding: 0 10px
   height: 32px
