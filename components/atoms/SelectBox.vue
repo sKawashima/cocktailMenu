b3403f0d48735ade77b24a5242c351180a7cb003
Shota Kawashima <bysk.333@gmail.com>
2019-08-07 23:17:34 +0900

    makeSelectBox (#104)
    * feat:create pull request
    
    * 配列の渡し方がわからない
    
    * 配列倒した
    
    * propsに型指定を追加

---
+++ components/atoms/SelectBox.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+select.select
+  option(v-for='choice in choices') {{ choice }}
+</template>
+
+<style lang="sass" scoped>
+$theme: #2D1112
+
+.select
+  color: $theme
+  border: 1px solid $theme
+  height: 32px
+  width: 100%
+</style>
+
+<script>
+export default {
+  props: {
+    choices: Array
+  }
+}
+</script>
--- stories/atoms/index.stories.js
+++ stories/atoms/index.stories.js
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/vue'
 import Label from '../../components/atoms/Label.vue'
 import Input from '../../components/atoms/Input.vue'
+import SelectBox from '../../components/atoms/SelectBox.vue'
 
 storiesOf('atoms/Label', module)
   .add('simple', () => ({
@@ -16,4 +17,12 @@ storiesOf('atoms/Input', module)
       Input
     },
     template: `<Input />`
+  }))
+
+storiesOf('atoms/SelectBox', module)
+  .add('simple', () => ({
+    components: {
+      SelectBox
+    },
+    template: `<SelectBox v-bind:choices='["1st", "2nd", "3rd"]'/>`
   }))
