6ec0e651452f40485dfcfb132064973ce892b32d
sKawashima <bysk.333@gmail.com>
2019-08-17 23:48:58 +0900

    だめ

--- scraping/index.ts
+++ scraping/index.ts
@@ -1,1 +1,34 @@
-console.log('hello')
+import puppeteer from 'puppeteer'
+
+const getCocktailList = async (page, url) => {
+  await page.goto(
+    "http://www.sakelog.com/cocktails/search/%E3%82%B8%E3%83%B3/1"
+  )
+  const elementsList = await page.evaluate(() => {
+    document.getElementsByClassName('itembox')
+    console.log(document.getElementsByClassName("itembox"))
+  })
+  // const cocktailList = await elementsList.map(element => {
+  //   return element.getElementsByTagName('a')[0].href
+  // })
+  return elementsList;
+}
+
+const test = async () => {
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    const cocktailList = await getCocktailList(
+      page,
+      'http://www.sakelog.com/cocktails/search/%E3%82%B8%E3%83%B3/1'
+    )
+    await console.dir(JSON.stringify(cocktailList))
+
+    browser.close()
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+test()
